47c25242724db1dad0436ce9673182a01849b9a9
Student <student@Techs-MacBook-Air.local>
2020-10-19 14:53:12 -0400

    EmployeeDAO

--- 06_Database_Connectivity_DAO/src/main/java/com/techelevator/projects/model/jdbc/JDBCEmployeeDAO.java
+++ 06_Database_Connectivity_DAO/src/main/java/com/techelevator/projects/model/jdbc/JDBCEmployeeDAO.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
+import com.techelevator.city.City;
 import com.techelevator.projects.model.Employee;
 import com.techelevator.projects.model.EmployeeDAO;
 
@@ -20,21 +22,53 @@ public class JDBCEmployeeDAO implements EmployeeDAO {
 	
 	@Override
 	public List<Employee> getAllEmployees() {
-		return new ArrayList<>();
+		
+		List<Employee> employeeList = new ArrayList<>();
+		String sql = "SELECT * FROM employee";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
+		
+		while (results.next()) {
+			Employee myemployee = mapRowToEmployee(results);
+			employeeList.add(myemployee);
+		}
+		
+		return employeeList;
 	}
 
 	@Override
 	public List<Employee> searchEmployeesByName(String firstNameSearch, String lastNameSearch) {
-		return new ArrayList<>();
+		
+		List<Employee> employeeList = new ArrayList<>();
+		String sql = "SELECT * FROM employee WHERE first_name = ? AND last_name = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, firstNameSearch, lastNameSearch);
+		
+		while (results.next()) {
+			Employee myemployee = mapRowToEmployee(results);
+			employeeList.add(myemployee);
+		}
+		
+		return employeeList;
 	}
 
 	@Override
 	public List<Employee> getEmployeesByDepartmentId(long id) {
-		return new ArrayList<>();
+		
+		List<Employee> employeeList = new ArrayList<>();
+		String sql = "SELECT * FROM employee WHERE department_id = ?";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, id);
+		
+		while (results.next()) {
+			Employee myemployee = mapRowToEmployee(results);
+			employeeList.add(myemployee);
+		}
+		
+		return employeeList;
 	}
 
 	@Override
 	public List<Employee> getEmployeesWithoutProjects() {
+		
+		
 		return new ArrayList<>();
 	}
 
@@ -46,6 +80,19 @@ public class JDBCEmployeeDAO implements EmployeeDAO {
 	@Override
 	public void changeEmployeeDepartment(Long employeeId, Long departmentId) {
 		
+	}
+	
+	private Employee mapRowToEmployee (SqlRowSet results) {
+		Employee theEmployee = new Employee();
+		theEmployee.setId(results.getLong("employee_id"));
+		theEmployee.setDepartmentId(results.getLong("department_id"));
+		theEmployee.setFirstName(results.getString("first_name"));
+		theEmployee.setLastName(results.getString("last_name"));
+		theEmployee.setBirthDay(results.getDate("birth_date").toLocalDate());
+		theEmployee.setGender(results.getString("gender").charAt(0));
+		theEmployee.setHireDate(results.getDate("hire_date").toLocalDate());
+		
+		return theEmployee;
 	}
 
 }
